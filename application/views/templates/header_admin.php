cc8ce34a915e1def78b9bd4fba2ea9c547480528
Advansoftware <brunoantunes94@hotmail.com>
2017-12-16 18:38:16 -0200

    Interface menu header lateral alterada com adição de alguns links

--- application/views/templates/header_admin.php
+++ application/views/templates/header_admin.php
@@ -15,21 +15,28 @@
 		
 		<nav class="side-navbar">
 	<div class="sidenav-header d-flex align-items-center justify-content-center">
-		<div class="sidenav-header-inner text-center"><img src="<?php echo $url;?>imagens/admin.png" alt="person" class="img-fluid rounded-circle">
-			<h2>Admin</h2>
+		<div class="sidenav-header-inner  text-center"><img src="<?php echo $url;?>imagens/logo.png" alt="CEP" class="img-fluid rounded-circle">
+			<h2>CEP - Admin</h2>
 		</div>
 		<div class="sidenav-header-logo"><a href="index.html" class="brand-small text-center">
-			<strong>A</strong><strong class="text-primary">D</strong></a>
+			<strong>CEP</strong></a>
 		</div>
 	</div>
 	<div class="main-menu">
 		<ul id="side-main-menu" class="side-menu list-unstyled">                  
 			<li>
-				<a href="<?php echo $url; ?>index.php/admin/dashboard" > <i class="icon-home"></i><span>Leads</span></a>
+				<a href="<?php echo $url; ?>index.php/admin/dashboard" > <i class="icon-home"></i><span>Curso</span></a>
 			</li>
 			<li>
-				<a href="<?php echo $url; ?>index.php/admin/estatistica" ><i class="icon-form"></i><span>Estatísticas</span></a>
+				<a href="<?php echo $url; ?>index.php/admin/estatistica" ><i class="icon-form"></i><span>Turma</span></a>
 			</li>
+			<li>
+				<a href="<?php echo $url; ?>index.php/admin/estatistica" ><i class="icon-form"></i><span>Disciplina</span></a>
+			</li>
+			<li>
+				<a href="<?php echo $url; ?>index.php/admin/estatistica" ><i class="icon-form"></i><span>Aluno</span></a>
+			</li>
+
 		</ul>
 	</div>
 </nav>
