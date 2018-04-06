a3c013e5dc70fbc027a48bafe45abf821c8015dc
Tadeu Reno <tadeu5552010@hotmail.com>
2018-04-06 01:59:00 -0300

    teste2 atom

--- application/controllers/Grupo.php
+++ application/controllers/Grupo.php
@@ -13,8 +13,8 @@
 			$this->set_menu();
 			$this->data['controller'] = get_class($this);
 			$this->data['menu_selectd'] = $this->Geral_model->get_identificador_menu(strtolower(get_class($this)));
-		} 
-
+		}
+ 
 		/*
 			Renderiza o dashboard
 		*/
